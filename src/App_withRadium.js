03329c62d9e6a13e8433625f99dccc1bd1bb4f80
Serena Gabbio <gabbio.serena@gmail.com>
2019-04-12 11:33:58 +0200

    modification before eject for css module

--- src/App_withRadium.js
+++ src/App_withRadium.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -50,7 +51,11 @@ class App extends Component {
             color: 'white',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }
         };
 
         let persons = null;
@@ -69,7 +74,10 @@ class App extends Component {
                 </div>
             );
             style.backgroundColor = 'red';
-
+            style[':hover'] = {
+                backgroundColor: 'lightcoral',
+                color: 'black'
+            };
         }
 
         let classes = [];
@@ -81,22 +89,24 @@ class App extends Component {
         }
 
         return (
-            <div className="App">
-                <h1>
-                    Hello!
-                </h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <button
-                    style={style}
-                    onClick={this.togglePersonsHandler}>Switch Name</button>
+            <StyleRoot>
+                <div className="App">
+                    <h1>
+                        Hello!
+                    </h1>
+                    <p className={classes.join(' ')}>This is really working!</p>
+                    <button
+                        style={style}
+                        onClick={this.togglePersonsHandler}>Switch Name</button>
 
-                {persons}
-            </div>
+                    {persons}
+                </div>
+            </StyleRoot>
         );
     }
 }
 
-export default App;
+export default Radium(App);
 
 
 
